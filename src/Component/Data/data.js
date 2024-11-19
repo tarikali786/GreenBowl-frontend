7e72238a59c28800398467e03590c855f192322a
Tariq Ali <tarikali7444@gmail.com>
2024-11-19 15:13:31 +0530

    add some image

--- src/Component/Data/data.js
+++ src/Component/Data/data.js
@@ -2,7 +2,19 @@ import Img1 from "../../assets/Home/OIG4.BZnusp1XJjNl52yMuPuy.jpg";
 import Img2 from "../../assets/Home/OIG4.ip.SqrbdnnV_TEPi2_LJ.jpg";
 import Img3 from "../../assets/Home/OIG4.JOLokemYsl1tf2u.upgi.jpg";
 import Img4 from "../../assets/Home/OIG4.t3buBNyKTDyhjGYfyDkN.jpg";
+import Img5 from "../../assets/Home/istockphoto-528360281-612x612.jpg";
+import Img6 from "../../assets/Home/Screenshot 2024-11-19 150828.png";
+import Img7 from "../../assets/Home/Screenshot 2024-11-19 150917.png";
 export const HomeHeroData = [
+  {
+    img: Img5,
+  },
+  {
+    img: Img6,
+  },
+  {
+    img: Img7,
+  },
   {
     img: Img1,
   },
